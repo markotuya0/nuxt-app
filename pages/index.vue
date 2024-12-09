94757cbc3852e25f7010b017c36602723ee1dee5
Mark Otuya <markotuya0@gmail.com>
2024-12-09 09:05:26 +0100

    added meta tag

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,11 @@
+<script setup>
+useHead({
+  title: "Home page",
+  script: ["main.js"],
+  meta: [{ name: "description", content: "this website is about nuxtjs" }],
+});
+</script>
 <template>
   <h1>Home page</h1>
-  <NuxtLink to="/about"> Go to About</NuxtLink>
+  <NuxtLink to="/about"> Go to About Page</NuxtLink>
 </template>
