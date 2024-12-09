76cef0ae716a11366e01fa1a1e50a86955f3256d
Mark Otuya <markotuya0@gmail.com>
2024-12-09 08:56:32 +0100

    Fetched data from github with github users api

---
+++ pages/github-user.vue
@@ -0,0 +1,9 @@
+<script setup>
+const { data: user } = useFetch("https://api.github.com/users/markotuya0");
+</script>
+<template>
+  <h1>{{ user?.name }}</h1>
+  <p>{{ user?.blog }}</p>
+  <p>{{ user?.bio }}</p>
+  <p>Public Repos = {{ user?.public_repos }}</p>
+</template>
